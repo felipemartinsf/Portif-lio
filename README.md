c78dee43d65f32cdec2d22544fb7cda222a5dc9a
Martins <fmartins@tifs.com>
2023-09-12 11:04:29 -0300

    1

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Resumo de informações minhas como programador.
+Funciona no mobile também :)
+
---
+++ index.js
@@ -0,0 +1,32 @@
+function descer1 (){
+    element = document.getElementById("first");
+    element.scrollIntoView({block: "center", inline: "center", behavior: 'smooth'
+});
+}
+
+function descer2 (){
+    element = document.getElementById("taxa2");
+    element.scrollIntoView({block: "center", inline: "center", behavior: 'smooth'
+});
+}
+
+function descer3 (){
+    element = document.getElementById("last");
+    element.scrollIntoView({block: "center", inline: "center", behavior: 'smooth'
+});
+}
+function link (){
+    var win = window.open('https://www.unifesp.br/', '_blank');
+    win.focus();
+}
+
+let a = document.getElementById('card1')
+let b = document.getElementById('card2')
+let c = document.getElementById('card3')
+let d = document.getElementById('b')
+let e = document.getElementById('logo')
+
+a.addEventListener("click",descer1);
+b.addEventListener("click",descer2);
+c.addEventListener("click",descer3);
+e.addEventListener("click",link)
