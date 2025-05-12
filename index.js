37a60ebb77863c139a8ec3c8bdaf80cd6dfc3348
Your Name <you@example.com>
2025-05-12 18:01:47 -0300

    design change and optimization

--- index.js
+++ index.js
@@ -1,79 +1,9 @@
-function descer1 (){
-    element = document.getElementById("first");
-    element.scrollIntoView({block: "center", inline: "center", behavior: 'smooth'
-});
+function calcularAno() {
+  const anoAtual = new Date().getFullYear();
+  const anoInicio = 2022;
+  const anosDeCurso = anoAtual - anoInicio +1;
+  document.getElementById("ano-contador").innerText = anosDeCurso;
 }
 
-function descer2 (){
-    element = document.getElementById("taxa2");
-    element.scrollIntoView({block: "center", inline: "center", behavior: 'smooth'
-});
-}
+calcularAno();
 
-function descer3 (){
-    element = document.getElementById("last");
-    element.scrollIntoView({block: "center", inline: "center", behavior: 'smooth'
-});
-}
-function link (){
-    var win = window.open('https://www.unifesp.br/', '_blank');
-    win.focus();
-}
-function firstOption(){
-  mostrarDiv()
-  esconderDiv2()
-}
-function mostrarDiv() {
-    var minhaDiv = document.getElementById("minhaDiv");
-    minhaDiv.style.display = "inline-block";
-    setTimeout(function() {
-      minhaDiv.style.opacity = "1";
-    }, 10); // Adicione um pequeno atraso para garantir que a transição ocorra após a exibição
-  }
-  
-  function esconderDiv() {
-    var minhaDiv = document.getElementById("minhaDiv");
-    minhaDiv.style.opacity = "0";
-    setTimeout(function() {
-      minhaDiv.style.display = "none";
-    }, 500); // Atraso de 500ms, correspondente à duração da transição
-  }
-
-  function mostrarDiv2() {
-    var minhaDiv = document.getElementById("minhaDiv2");
-    minhaDiv.style.display = "inline-block";
-    minhaDiv.style.positon = "absolute";
-    setTimeout(function() {
-      minhaDiv.style.opacity = "1";
-    }, 10); // Adicione um pequeno atraso para garantir que a transição ocorra após a exibição
-  }
-  
-  function esconderDiv2() {
-    var minhaDiv = document.getElementById("minhaDiv2");
-    minhaDiv.style.opacity = "0";
-    setTimeout(function() {
-      minhaDiv.style.display = "none";
-    }, 500); // Atraso de 500ms, correspondente à duração da transição
-  }
-  
-
-/*
-var cursor = document.getElementById('cursor');
-document.addEventListener('mousemove', function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.left = x + "px"
-    cursor.style.top = y + "px"
-})
-*/
-
-let a = document.getElementById('card1')
-let b = document.getElementById('card2')
-let c = document.getElementById('card3')
-let d = document.getElementById('b')
-let e = document.getElementById('logo')
-
-a.addEventListener("click",descer1);
-b.addEventListener("click",descer2);
-c.addEventListener("click",descer3);
-e.addEventListener("click",link)
