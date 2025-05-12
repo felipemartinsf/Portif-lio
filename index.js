5ae43e38f8fb57209de2c84fcbc3cb28ff05e378
Your Name <you@example.com>
2025-05-12 20:01:34 -0300

    last details

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function calcularAno() {
 
 calcularAno();
 
+function mostrarData() {
+    const dataAtual = new Date(); 
+    const mes = dataAtual.getMonth() + 1; 
+    const ano = dataAtual.getFullYear();
+
+    const mesFormatado = mes < 10 ? `0${mes}` : mes;
+
+    document.getElementById("data-atual").innerText = `${mesFormatado}/${ano}`;
+}
+
+mostrarData();
